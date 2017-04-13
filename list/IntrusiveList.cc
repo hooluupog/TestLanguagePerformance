478afa8591bbd2582fc5fda3263117c997a18239
hooluupog <qq510371827@gmail.com>
2017-04-13 22:28:18 +0800

    style

--- list/IntrusiveList.cc
+++ list/IntrusiveList.cc
@@ -17,121 +17,121 @@ public:
 	class iterator {
 	public:
 		iterator(ListNode* node) :
-				_node(node) {
+				node_(node) {
 		}
 		iterator& operator ++() {
-			if (_node) {
-				_node = _node->_next;
+			if (node_) {
+				node_ = node_->next_;
 			}
 			return *this;
 		}
 		iterator& operator --() {
-			if (_node) {
-				_node = _node->_prev;
+			if (node_) {
+				node_ = node_->prev_;
 			}
 			return *this;
 		}
 		bool operator ==(const iterator& iter) {
-			return _node == iter._node;
+			return node_ == iter.node_;
 		}
 		bool operator !=(const iterator& iter) {
 			return !operator ==(iter);
 		}
 		T& operator *() {
-			return *static_cast<T*>(_node);
+			return *static_cast<T*>(node_);
 		}
 
 		T* operator ->() {
-			return _node;
+			return node_;
 		}
 	private:
-		ListNode* _node;
+		ListNode* node_;
 	};
 
 	struct ListNode {
 		template<class ... Args>
 		ListNode() :
-				_list(nullptr), _prev(nullptr), _next(nullptr) {
+				list_(nullptr), prev_(nullptr), next_(nullptr) {
 		}
-		LinkList* _list;
-		ListNode* _prev;
-		ListNode* _next;
+		LinkList* list_;
+		ListNode* prev_;
+		ListNode* next_;
 	};
 
 	LinkList() :
-			_head(new ListNode()), _len(0) {
-		_head->_list = this;
-		_head->_prev = _head;
-		_head->_next = _head;
+			head_(new ListNode()), len_(0) {
+		head_->list_ = this;
+		head_->prev_ = head_;
+		head_->next_ = head_;
 	}
 
 	~LinkList() {
-		ListNode* node = _head->_next;
-		while (node != _head) {
-			_head->_next = node->_next;
-			delete node;
-			node = _head->_next;
+		ListNode* node_ = head_->next_;
+		while (node_ != head_) {
+			head_->next_ = node_->next_;
+			delete node_;
+			node_ = head_->next_;
 		}
-		delete _head;
+		delete head_;
 	}
 
 	template<class ... Args>
 	T* getFirst() {
-		return (T*) _head->_next;
+		return (T*) head_->next_;
 	}
 	T* getLast() {
-		return _head->_prev;
+		return head_->prev_;
 	}
 	bool isEmpty() {
-		return _len == 0;
+		return len_ == 0;
 	}
 	int length() {
-		return _len;
+		return len_;
 	}
 
 	void add(T* t) {
 		// append [t] into LinkList.
-		_insert(t, _head->_prev);
+		insert(t, head_->prev_);
 	}
 
 	void addFirst(T* t) {
 		// add [t] at the first.
-		_insert(t, _head);
+		insert(t, head_);
 	}
 
 	void insertBefore(T* t, T* at) {
 		// Insert [t] before [at].
-		_insert(t, ((ListNode*) at)->_prev);
+		insert(t, at->prev_);
 	}
 
 	void insertAfter(T* t, T* at) {
 		// Insert [t] after [at].
-		_insert(t, at);
+		insert(t, at);
 	}
 
-	void _insert(T* t, ListNode* at) {
-		if (t->_list != nullptr) {
+	void insert(T* t, ListNode* at) {
+		if (t->list_ != nullptr) {
 			throw std::runtime_error("ListNode is already in a LinkList.");
 		}
-		t->_list = this; // Link value into LinkList.
-		t->_prev = at;
-		t->_next = at->_next;
-		at->_next = t;
-		t->_next->_prev = t;
-		_len++;
+		t->list_ = this; // Link value into LinkList.
+		t->prev_ = at;
+		t->next_ = at->next_;
+		at->next_ = t;
+		t->next_->prev_ = t;
+		len_++;
 	}
 
 	T* remove(T* t) {
-		t->_prev->_next = t->_next;
-		t->_next->_prev = t->_prev;
-		t->_list = nullptr;
-		t->_prev = t->_next = nullptr;
-		_len--;
+		t->prev_->next_ = t->next_;
+		t->next_->prev_ = t->prev_;
+		t->list_ = nullptr;
+		t->prev_ = t->next_ = nullptr;
+		len_--;
 		return t;
 	}
 
 	void reverseBetween(int m, int n) {
-		if (m < 1 || n > _len) {
+		if (m < 1 || n > len_) {
 			std::string str = "Index out of bounds. Wrong range ("
 					+ std::to_string(m) + "," + std::to_string(n) + ")";
 			throw std::range_error(str);
@@ -139,13 +139,13 @@ public:
 		int cnt = n - m; // 1 <= m <= n <= LinkList.Length
 		T* start = getFirst();
 		for (int i = 1; i < m; i++) {
-			//start points to the first node to be reversed.
-			start = (T*) start->_next;
+			//start points to the first node_ to be reversed.
+			start = (T*) start->next_;
 		}
 		T* end = start;
 		while (cnt > 0) {
-			// Insert nodes from m to n into list.
-			T* curr = (T*) end->_next;
+			// Insert node_s from m to n into list_.
+			T* curr = (T*) end->next_;
 			remove(curr);
 			insertBefore(curr, start);
 			start = curr;
@@ -154,23 +154,23 @@ public:
 	}
 
 	iterator begin() {
-		return iterator(_head->_next);
+		return iterator(head_->next_);
 	}
 	iterator end() {
-		return iterator(_head);
+		return iterator(head_);
 	}
 
 private:
-	ListNode* _head;
-	int _len;
+	ListNode* head_;
+	int len_;
 };
 
 template<class T>
 struct Slist: public LinkList<Slist<T>>::ListNode {
 	Slist(T val) :
-			val(val) {
+			val_(val) {
 	}
-	T val;
+	T val_;
 };
 
 int main() {
@@ -192,7 +192,7 @@ int main() {
 		L.addFirst(new Slist<int>(nums[i]));
 	}
 	for (auto i : L) {
-		std::cout << i.val << ' ';
+		std::cout << i.val_ << ' ';
 	}
 	std::cout << std::endl;
 	L.reverseBetween(3, 8);
